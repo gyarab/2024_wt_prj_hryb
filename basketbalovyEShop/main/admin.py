fbe7797177d5dea5d32d2aea1b377dca0fc2bf68
Andrii Hryb <andrii.hryb.s@gyarab.cz>
2025-03-21 12:32:05 +0100

    update

--- basketbalovyEShop/main/admin.py
+++ basketbalovyEShop/main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Produkt, Brand
+
+class ProduktAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "price", "brand"]
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "year"]
 
 # Register your models here.
+admin.site.register(Produkt, ProduktAdmin)
+admin.site.register(Brand, BrandAdmin)
--- basketbalovyEShop/main/models.py
+++ basketbalovyEShop/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Produkt(models.Model):
+    title = models.CharField(max_length=300)
+    price = models.IntegerField(null=True, blank=True)
+    description = models.TextField(blank=True, default="")
+    brand = models.ForeignKey('Brand', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"
+
+class Brand(models.Model):
+    name = models.CharField(max_length=300)
+    year = models.IntegerField(null=True, blank=True)
+    description = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
